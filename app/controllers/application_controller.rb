a7b5fccea7d47cf7a840038665d2ca8842daf7a1
bangn <thebangnguyen@gmail.com>
2024-11-23 19:20:42 +1100

    Fix "Your browser is not supported"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # https://discuss.rubyonrails.org/t/help-rails-hello-world-complains-of-your-browser-is-not-supported-please-upgrade-your-browser-to-continue-when-displaying-simple-html/86744
+  # allow_browser versions: :modern
   protect_from_forgery with: :exception
 end
