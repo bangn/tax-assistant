b73204c3bdd094a8257b0b5ecda8be5a3cf14db4
bangn <thebangnguyen@gmail.com>
2024-11-23 19:04:16 +1100

    Remove dashboard resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "home#show"
-  get "/dashboard" => "dashboard#show"
   get "/auth/auth0/callback" => "auth0#callback"
   get "/auth/failure" => "auth0#failure"
   get "/auth/logout" => "auth0#logout"
